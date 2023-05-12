9c63c6f25ffb08bbc9d1ce48c62792437638fbe8
deepshikha <deepshikhaverma44@gmail.com>
2023-05-12 10:45:39 +0530

    Sum of dgt inside string

--- sumwithinString.js
+++ sumwithinString.js
@@ -1,3 +1,11 @@
 var s = 'f12r3id45ay';
-let arr = parseInt(s.replace(/[^0-9]/g,""));
-console.log(arr);
+let dgt = parseInt(s.replace(/[^0-9]/g,""));
+let sum = 0;
+let rev = 0;
+console.log('Num ='+dgt);
+while(dgt>0){
+    rev = dgt%10;
+    sum = sum + rev;
+    dgt = Math.floor(dgt/10);
+}
+console.log('Sum of digits in string ='+sum);
