b4d680e1188973f7cf843a6d2d46d02faf4c04e7
deepshikha <deepshikhaverma44@gmail.com>
2024-01-26 19:25:38 +0530

    redux with nodeJs

---
+++ redux/index.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux';
+const store = createStore(reducer);
+// reducer
+function reducer(state = { amount: 1000 }, action) {
+    return state;
+}
+
+console.log(store.getState());
