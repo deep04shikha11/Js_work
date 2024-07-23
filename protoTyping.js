9e8ece6eb673e529bff0883346dbb3df47686f1d
deep04shikha11 <deepshikhaverma44@gmail.com>
2024-07-23 22:20:47 +0530

    Simple example of proto_Typing

--- protoTyping.js
+++ protoTyping.js
@@ -1,10 +1,33 @@
+/* 
+
 function Person(name){
     this.name = name;
 }
-
 Person.prototype.sayHello = function(){
     console.log(`Hello ${this.name} !`);
 }
-
 var prsn_obj = new Person("DeepShikha");
-prsn_obj.sayHello();
+prsn_obj.sayHello(); 
+
+*/
+
+function Shape() {
+    this.name = "Sqr Shape";
+}
+Shape.prototype.getName = function () {
+    return this.name;
+}
+function Square(side) {
+    Shape.call(this);
+    this.side = side;
+}
+Square.prototype = Object.create(Shape.prototype);
+Square.prototype.constructor = Square;
+Square.prototype.getArea = function () {
+    return this.side * this.side;
+}
+var sqr = new Square(5);
+console.log(sqr.getName());
+
+
+
