1422badcaaa8aebb86946e7ec9ad109839a2ec30
deep04shikha11 <deepshikhaverma44@gmail.com>
2024-08-14 15:13:02 +0530

    two pointers using binary search algorithm

---
+++ searchIndex.js
@@ -0,0 +1,26 @@
+// search - insert - position;
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (right >= left) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] > target) right = mid - 1;
+    else left = mid + 1;
+  }
+
+  return left;
+};
+
+// Example usage
+const nums = [1, 3, 5, 6];
+const target = 5;
+const Idx = searchInsert(nums, target);
+console.log("Insert Position:", Idx);
